e4a3508f20628be987a9952ef3e7b41c332761da
teenuputhenthopill@gmail.com <teenuputhenthopill@gmail.com>
2023-03-24 14:33:21 +0200

    Initial Commit

---
+++ RestartSwiftUI/RestartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartSwiftUIApp.swift
+//  RestartSwiftUI
+//
+//  Created by Teenu Abraham on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
